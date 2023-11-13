11949f87461a651749e16a4d6521c1703bfe2371
Bella <bella.kallen2014@gmail.com>
2023-11-13 22:08:30 +0300

    scr added

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -20,8 +20,8 @@ public class StatisticsServiceTest {
     void findMaxFromMiggle() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 35, 8, 6, 11, 11, 12};
-        long expected = 35;
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 20, 8, 6, 11, 11, 12};
+        long expected = 20;
 
         long actual = service.findMax(incomesInBillions);
 
